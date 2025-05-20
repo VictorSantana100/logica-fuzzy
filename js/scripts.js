2193b14feb41f254428905658559c104792c3e5c
Victor Santana <santanav589@gmail.com>
2025-05-19 23:25:33 -0300

    feat: Add Implementações

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+/**
+ * Recebe um array com elementos, no formato "v; 0.4" 
+ * retorna o somatório dos graus de pertinencia
+* @param {string[]} elements - Array de strings no formato "nome; valor"
+* @returns {number} Soma dos graus de pertinência extraídos
+*/
+function somaGrausPertinencia(elements) {
+    let somaGraus = 0;
+    elements.forEach(element => {
+        const elementSplit = splitElement(element);
+        somaGraus += parseFloat(elementSplit[1]);
+    });
+    return somaGraus;
+}
+
+/**
+ * Divide uma string no formato "chave; valor" em duas partes,
+ * separando a chave (ex: variável) do valor (ex: grau de pertinência).
+ *
+ * @param {string} element - String no formato "chave; valor"
+ * @returns {string[]} Um array com duas posições: [chave, valor]
+ */
+function splitElement(element) {
+    return element.split(';');
+}
+
+
+let elementsI = ["v; 0.4", "w; 0.2", "x; 0.5", "y; 0.4", "z; 1"];
+let solutionI = document.getElementById('solutionI');
+solutionI.innerHTML = somaGrausPertinencia(elementsI);
+
+let elementsII = ["x; 1", "y; 1", "z; 1"];
+let solutionII = document.getElementById('solutionII');
+solutionII.innerHTML = somaGrausPertinencia(elementsII);
